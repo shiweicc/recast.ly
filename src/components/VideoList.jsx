5f0c7c47d0791c8cd8959d57737e18fe80c01387
Shiwei Chen <shiweichenus@gmail.com>
2022-04-07 14:19:11 -0500

    Create a dynamic Video Player component

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,55 +1,25 @@
 import exampleVideoData from '../data/exampleVideoData.js';
+import VideoListEntry from './VideoListEntry.js';
+
 // what's the relationship between videolistentry and view goes here
 // what's props in this case, do we use props, and does that referring to examplevideodata
 //
 
-// ORIGINAL
-<div className="video-list">
-  <div><h5><em>videoListEntry</em> view goes here</h5></div>
-  <div><h5><em>videoListEntry</em> view goes here</h5></div>
-  <div><h5><em>videoListEntry</em> view goes here</h5></div>
-  <div><h5><em>videoListEntry</em> view goes here</h5></div>
-  <div><h5><em>videoListEntry</em> view goes here</h5></div>
-</div>;
-
-// ATTEMPT 1
-// class VideoList extends React.Component {
-//   constructor(props) {
-//     super(props);
-//   }
-
-//   renderVideo (entry) {
-//     return (
-//       <videoListEntry title={entry.title} description={entry.description}/>
-//     )
-//   }
-
-//   render () {
-//     exampleVideoData.map(renderVideo)
-//     return (
-//       <div></div>
-//     )
-//   }
-// }
-
-// ATTEMPT 2
 var VideoList = (props) => {
-  console.log(props);
-  // var renderVideo = function (entry) {
-  //   return (
-  //     <videoListEntry title={entry.title} description={entry.description}/>
-  //   );
-  // };
 
-  exampleVideoData.map(renderVideo);
+  const videos = props.videos.map(function(element, index) {
+    return (
+      <VideoListEntry key={element.etag} video={element} />
+    );
+  });
+
   return (
-    <div></div>
+    <div className="video-list">{videos}</div>
   );
 };
 
 
 // ----------
-
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
 VideoList.propTypes = {
